1efa5be7f9f042fc532a8d5d9ff4fef748a8b00b
uplavikarnitish <uplavikarnitish@gmail.com>
2015-06-08 14:50:40 -0500

    Working!!

--- src/tutorial/Searcher.java
+++ src/tutorial/Searcher.java
@@ -1,6 +1,14 @@
 package tutorial;
 
+import com.sun.org.apache.xpath.internal.SourceTree;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.SimpleFSDirectory;
 import org.apache.lucene.index.DirectoryReader;
@@ -22,9 +30,26 @@ public class Searcher {
         search(indexDir, q);
     }
 
-    public static void search(String indexDir, String q) throws IOException {
+    public static void search(String indexDir, String q) throws IOException, ParseException {
         Directory dir = new SimpleFSDirectory(Paths.get(indexDir));
-        IndexSearcher is = new IndexSearcher( DirectoryReader.open(indexDir));
+        //IndexSearcher is = new IndexSearcher( DirectoryReader.open(indexDir));  IMP
+        IndexSearcher is = new IndexSearcher(DirectoryReader.open(dir));
+
+        QueryParser parser = new QueryParser("contents", new StandardAnalyzer());
+        Query query = parser.parse(q);
+        long start = System.currentTimeMillis();
+        TopDocs hits = is.search(query, 10);
+        long end = System.currentTimeMillis();
+        System.err.println("Found " + hits.totalHits + "; Document(s) (in " + (end - start) + " milliseconds) that matched query '" + q + "' ;");
+
+        for (int i = 0; i<hits.totalHits; i++)
+        {
+            ScoreDoc scoreDoc = hits.scoreDocs[i];
+            Document doc = is.doc(scoreDoc.doc);
+            System.out.println("<#"+String.format("%02d", (i+1))+"> "+doc.get("filename")+"<ID:"+scoreDoc.doc+">"+" score:"+scoreDoc.score);
+        }
+
+
 
     }
 }
